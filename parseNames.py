b0b95b743bec257c14104dea647b9aea5ef3755f
Jim Rowland <jim.rowland347@gmail.com>
2016-07-17 11:24:51 -0400

    Added parseName.py to parse a name of the form "Last, First"

---
+++ parseNames.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 17 10:49:47 2016
+
+@author: jrr
+"""
+
+def parse_student_name(str):
+    ''' given a student'name in the form last,first
+        parse the string and return a tuple with (first last)
+    '''
+    names = str.split(',')
+    if len(names) >= 2:
+        return (names[1].strip(), names[0].strip())
+    else:
+        return names[0]
+        
+print(parse_student_name('Rowland, Jim'))
